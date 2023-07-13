ad98fe6b2da7f75c664b623478e7dd97af56dead
Dmitriy Kozak <dmitrij.kozak.1997@bk.ru>
2023-07-13 10:25:08 +0400

    Gitignore added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,11 +19,11 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxIfCurrentMaxMoreIncome() {
-        StatisticsService service = new StatisticsService();
+    void findMax2() {
+            StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 21};
-        long expected = 21;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
